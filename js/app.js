318592bf5cde2e8941941060ce301952e7f85a83
Lauraisa <galindolaura.isabel@gmail.com>
2017-12-21 10:27:18 -0500

    dando funcinalidad al boton-1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,49 @@
-jQuery(document).ready(function() {
-  console.log(jQuery('#p-test').text());
-  console.log(jQuery('#p-test').html());
-  console.log(jQuery('#input-test').val());
-});
+/*function begin() {
+  jQuery('#p-test').click(function() {
+    jQuery(this).hide();
+  })
+
+  jQuery('#boton-1').on('click', function() {
+    jQuery('#p-test').show();
+  })
+
+  $('#boton-2').click(function() {
+    $('#p-test').hide('fast');
+  })
+
+  $('#boton-3').click(function() {
+    $('#p-test').show('slow');
+  })
+
+  $('#boton-4').click(function() {
+    $('#p-test').toggle();
+  })
+
+  $('#boton-5').click(function() {
+    $('#p-test').toggle('swing');
+  })
+
+  // $('#p-test').css('background-color', 'yellow');
+  /* $('#p-test').css({
+    backgroundColor: 'yellow',
+    border: '1px solid black'
+  }); */
+//}
+
+
+jQuery(document).ready();
+
+
+$('p').on('click', function() {
+  $('p').hide();
+})
+/*dando funcionalidad aal boton-1*/
+
+$('input:eq(0)').on('click', function() {
+  $('p').show();
+})
+
+/* u otra bforma seria */
+$('#boton-1)').on('click', function() {
+  $('p').show();
+})
